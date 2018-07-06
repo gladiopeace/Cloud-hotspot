d08c62b763bae9fdb9ffbbfc59b44908b792387f
Ethan Yang <345548528@qq.com>
2018-07-06 14:26:38 +0800

    update

--- application/language/chinese/signup_lang.php
+++ application/language/chinese/signup_lang.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['sign_up'] = "注册";
 $lang['sign_in'] = "登录";
 $lang['reset_pass'] = "忘记密码";
-$lang['account_fill'] = "邮箱,手机号";
+$lang['account_fill'] = "邮箱地址";
 $lang['password_fill'] = "请输入密码";
 $lang['re_password_fill'] = "确认密码";
 $lang['verify_code'] = "获取验证码";
