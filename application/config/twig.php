3b9dfde2ffba4270c3bb4f76da0efe3b19e51261
Ethan.Yang <345548528@qq.com>
2023-04-23 15:34:13 +0800

    Update
    Update

--- application/config/twig.php
+++
@@ -1,5 +0,0 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
- 
-$config['template_dir'] = APPPATH.'views/';
-$config['portal_template'] = FCPATH.'template/';
-$config['cache_dir'] = FCPATH.'data/cache/twig';
